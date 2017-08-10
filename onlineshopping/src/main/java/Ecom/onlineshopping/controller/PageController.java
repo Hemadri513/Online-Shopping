418afa6d745281834156567b97c0dda73e566935
Hemadri <Hemadri@LAPTOP-A1BLSAP6>
2017-08-10 23:19:24 +0530

    Exception handling is added, it works properly

--- onlineshopping/src/main/java/Ecom/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/Ecom/onlineshopping/controller/PageController.java
@@ -1,5 +1,7 @@
 package Ecom.onlineshopping.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,7 @@ import Ecom.shopingbackend.dto.Product;
 @Controller
 public class PageController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(PageController.class);
 	
 	@Autowired
 	private CategoryDAO categoryDAO;
@@ -28,6 +31,9 @@ public class PageController {
 		
 		mv.addObject("title","Home");
 		//passing the list of categories
+		
+		logger.info("Inside PageController index method - INFO");
+		logger.debug("Inside PageController index method- DEBUG");
 		
 		mv.addObject("categories", categoryDAO.list());
 		
