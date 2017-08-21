92ac849f841158ff2fdaff93b942967186b264df
Hemadri <Hemadri@192.168.1.20>
2017-08-21 13:16:21 +0530

    cart products are updated perfectly

--- onlineshopping/src/main/webapp/WEB-INF/views/about.jsp
+++ onlineshopping/src/main/webapp/WEB-INF/views/about.jsp
@@ -4,7 +4,17 @@
 	
 		<h1>About us</h1>
 		<hr/>
-		<p>this is an about us page</p>
+		<p>Thank you for shopping at DudesGadget.  Here at DudesGadget our goal is to deliver the coolest and most unique products everyday from all around the globe...
+
+We know how something special brightens up your day. Please contact us if you have any questions or comments. 
+
+Don't forget to share the love by sharing our store!
+
+Cheers,
+
+Dudes Gadget Team
+
+hello@dudesgadget.com</p>
 	
 	</div>
 
--- onlineshopping/src/main/webapp/assets/js/myapp.js
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -434,10 +434,54 @@ if($categoryForm.length) {
 
 
 //-----------------
+// handling the click event of refresh cart button
+
+$('button[name="refreshCart"]').click(function(){
+
+	// fetch the cart line id
+	var cartLineId = $(this).attr('value');
+	var countElement = $('#count_' + cartLineId);
+	
+	var originalCount = countElement.attr('value');
+	
+	var currentCount = countElement.val();
+	
+	// work only when the count has changed
+	
+	if(currentCount !== originalCount) 
+	{
+		
+		if(currentCount < 1 || currentCount > 5) {
+			//reverting back to the original count
+			//user has given value below 1 and above 5
+			
+			countElement.val(originalCount);
+			bootbox.alert({
+				size: 'medium',
+				title: 'Error',
+				message: 'Product count should be minimum 1 and maximum 5!'
+			});
+		}
+		
+		else {
+			 
+			var updateUrl = window.contextRoot + '/cart/' + cartLineId + '/update?count=' + currentCount;
+			// forward if to the controller
+			
+			window.location.href = updateUrl;
+		}
+	}
 	
 	
 	
 	
+});
+
+
+
 	
+	
+	
+	//-------------------------
 	
 });
